38b7a6f25d2759794e789bdd65ca6b2d87fb5f66
pbicskei <p.bicskei@gmail.com>
2021-02-15 11:54:38 +0100

    fix: generalizing to tkhcloud vars

--- variables.tf
+++ variables.tf
@@ -1,19 +1,16 @@
-variable "aws_region" {
-  type    = string
-  default = "us-west-1"
-}
+# variable "aws_region" { type = string }
 
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
-}
+# variable "db_table_name" {
+#   type    = string
+#   default = "terraform-learn"
+# }
 
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
+# variable "db_read_capacity" {
+#   type    = number
+#   default = 1
+# }
 
-variable "db_write_capacity" {
-  type    = number
-  default = 1
-}
+# variable "db_write_capacity" {
+#   type    = number
+#   default = 1
+# }
