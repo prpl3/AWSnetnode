41475dc7ff46b5315b906823baa09fc46894df3b
pbicskei <p.bicskei@gmail.com>
2021-02-15 12:56:18 +0100

    fixed output reference s3

--- project-outputs.tf
+++ project-outputs.tf
@@ -2,6 +2,6 @@ output "tfstate_table" {
   value = aws_dynamodb_table.tfstate_table.arn
 }
 output "tfstate_s3" {
-  value = aws_dynamodb_table.tfstate_s3.arn
+  value = aws_s3_bucket.tfstate_s3.arn
 }
 
