e95150718ca07c3df73d923945f1a9ff2513499e
pbicskei <p.bicskei@gmail.com>
2021-02-15 16:50:47 +0100

    moved more to more vars

--- projectVarPreload.tf
+++ projectVarPreload.tf
@@ -1,5 +1,5 @@
 ######################################################
-## Use TF Cloud variables or local terraform.tfvars ##
+## Use TF Cloud variables or local terraform.tfvars
 variable "aws_profile" { type = string }
 variable "default_aws_region" { type = string }
 variable "whoami" { type = string } 
@@ -8,3 +8,9 @@ variable "project_name" { type = string }
 variable "db_read_capacity" { type = number }
 variable "db_write_capacity" { type = number }
 ######################################################
+## VPC Specific
+variable "vpc_cidr" { type = string }
+variable "nr_azs" { type = number }
+
+variable "public_subnets" { }
+variable "private_subnets" { }
--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,14 @@
 data "aws_availability_zones" "available" {}
 
-#   data "aws_security_group" "default" {
-#   name   = "default"
-#   vpc_id = module.vpc.vpc_id
-# }
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = join("-",compact([ var.project_name,var.environment,var.whoami]))
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr
 
-  azs             = ["${var.default_aws_region}a", "${var.default_aws_region}b", "${var.default_aws_region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
