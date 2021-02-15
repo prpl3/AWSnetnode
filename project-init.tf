9fd198cc1ea4fb8c61303cf8952bb3f04d9d986e
pbicskei <p.bicskei@gmail.com>
2021-02-15 12:41:22 +0100

    chore: moving some more vars

--- project-init.tf
+++ project-init.tf
@@ -1,10 +1,9 @@
 ## TF Cloud variables ##############################
 variable "default_aws_region" { type = string }   ##
-variable "db_table_name" { type = string }        ##
+variable "project_name" { type = string }        ##
 variable "db_read_capacity" { type = number }     ##
 variable "db_write_capacity" { type = number }    ##
 ####################################################
-
 
 provider "aws" {
   region = var.default_aws_region
@@ -13,8 +12,8 @@ provider "aws" {
 resource "random_pet" "table_name" {}
 
 resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
+  name = join("-",compact([ var.project_name,var.ENVIRONMENT,random_pet.table_name.id])) 
+  
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
   hash_key       = "UUID"
@@ -29,4 +28,16 @@ resource "aws_dynamodb_table" "tfc_example_table" {
     name = "UserName"
     type = "S"
   }
+}
+
+resource "aws_s3_bucket" "terraform_state_s3_bucket" {
+    bucket = join("-",compact([ var.project_name,var.ENVIRONMENT,"state","files" ]))
+ 
+    versioning {
+      enabled = true
+    }
+ 
+    lifecycle {
+      prevent_destroy = true
+    }
 }
