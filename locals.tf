59018e9e1fabd61117b74ae763f1ae0332325980
pbicskei <p.bicskei@gmail.com>
2021-02-16 01:28:13 +0100

    fiddling around

---
+++ locals.tf
@@ -0,0 +1,59 @@
+# Here we generate some logical combinations for use in our terraform space
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
+
+locals {
+
+        vpc_name = join( 
+                        "-", compact([
+                                var.whoami,
+                                replace(var.principl3_tld,".","-"),
+                                ]))
+
+        vpc_url = join( 
+                        ".", compact([
+                                var.whoami,
+                                var.aws_region,
+                                var.principl3_tld
+                                ]))
+
+        storage_name = join( 
+                        "-", compact([
+                                "s3",
+                                var.aws_region,
+                                var.whoami,
+                                replace(var.storage_tld,".","-"),
+                                ]))
+        storage_url = join( 
+                        ".", compact([
+                                "s3",
+                                var.aws_region,
+                                var.whoami,
+                                var.storage_tld,
+                                ]))
+
+        instance_url = join( 
+                        ".", compact([
+                                var.whoami,
+                                var.aws_region,
+                                var.storage_tld,
+                                ]))
+
+        principl3_tag = join( 
+                        "/", compact([
+                                data.aws_caller_identity.current.arn,
+                                replace(local.vpc_name,"-",":")
+                                ]))
+
+        deployed_by = join( 
+                ",", compact(
+                        [ replace( data.aws_caller_identity.current.arn, join(
+                                "",[
+                                "arn:aws:iam::",
+                                data.aws_caller_identity.current.id,
+                                ":user/"])
+                        ,"")
+                        ] 
+                )
+        )    
+}
--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = local.principl3_tag
+  name = local.vpc_name
+
   cidr = var.vpc_cidr
-  # count = var.nr_azs
   azs             = data.aws_availability_zones.available.names
-  # public_subnets  = [cidrsubnet(var.vpc_cidr,8,count.index + 1 )] # ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets  = [cidrsubnet(var.vpc_cidr,8, 1 ),cidrsubnet(var.vpc_cidr,8, 2 ),cidrsubnet(var.vpc_cidr,8, 3 )] # ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
   # private_subnets = [cidrsubnet(var.vpc_cidr,8,count.index + 101 )] # ["172.16.101.0/24", "172.16.102.0/24", "172.16.103.0/24"]
 
   enable_nat_gateway = false
@@ -15,5 +15,8 @@ module "vpc" {
     Terraform = "true"
     Environment = local.principl3_tag
     Deployed_by = local.deployed_by
+    principl3_url = local.vpc_url
+    storage_name = local.storage_name
+    storage_url = local.storage_url
   }
 }
